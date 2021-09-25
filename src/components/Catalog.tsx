cb9d31a4ca669276556cfbcd41833c61bf92ffcf
sonegobruno <sonegobruno@gmail.com>
2021-09-25 14:01:38 -0300

    feat: Added redux saga

--- src/components/Catalog.tsx
+++ src/components/Catalog.tsx
@@ -1,11 +1,9 @@
-import { useCallback, useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useEffect, useState } from 'react';
 import { api } from '../services/api';
-import { addProductToCart } from '../store/modules/cart/action';
 import { IProduct } from '../store/modules/cart/types';
+import { CatalogItem } from './CatalogItem';
 
 export function Catalog() {
-    const dispatch = useDispatch();
     const [ catalog, setCatalog ] = useState<IProduct[]>([])
 
     useEffect(() => {
@@ -14,20 +12,11 @@ export function Catalog() {
         })
     },[])
 
-    const handleAddProductToCart = useCallback((product: IProduct) => {
-        dispatch(addProductToCart(product))
-    },[])
-
    return (
        <main>
            <h1>Catalog</h1>
             {catalog.map(product => (
-                <article key={product.id}>
-                    <strong>{product.title}</strong> {" - "}
-                    <span>{product.price}</span> {"   "}
-
-                    <button type="button" onClick={() => handleAddProductToCart(product)}>Comprar</button>
-                </article>
+                <CatalogItem key={product.id} product={product}/>
             ))}
        </main>
    );
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,7 +1,23 @@
-import { createStore } from 'redux';
+import { applyMiddleware, createStore } from 'redux';
+import createSagaMiddleware from 'redux-saga';
+
+import { ICartState } from './modules/cart/types';
 import rootReducer from './modules/rootReducer';
+import rootSaga from './modules/rootSaga';
+
+export interface IState {
+    cart: ICartState
+}
+
+const sagaMiddleare = createSagaMiddleware();
+
+const middlewares = [sagaMiddleare];
 
 //Informação que estará disponivel em toda aplicação
-const store = createStore(rootReducer)
+const store = createStore(rootReducer,
+    applyMiddleware(...middlewares))
+
+
+sagaMiddleare.run(rootSaga)
 
 export default store;
